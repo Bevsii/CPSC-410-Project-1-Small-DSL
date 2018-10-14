fd546126d5e7c6f98e531c81f89101cb1e78031b
Justin Tang <justin.tang@alumni.ubc.ca>
2018-10-14 14:30:07 -0700

    Parsing and most evaluations

--- src/ast/Statement.java
+++ src/ast/Statement.java
@@ -3,13 +3,4 @@ package ast;
 import libs.Node;
 
 public  abstract class Statement extends Node {
-    public static Statement getSubStatement(){
-        if (tokenizer.checkToken("set")) {
-            return new Set();
-        }
-        if (tokenizer.checkToken("get")){
-            return new Get();
-        }
-        else return null;
-    }
 }
